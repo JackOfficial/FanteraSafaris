036f13d14d4ea3bf044b6bf7c9e7229eed26a5f5
JackOfficial <musengimanajacques@gmail.com>
2026-01-29 15:37:37 +0200

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 })->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/hotel', function () {
+    return view('hotel');
+});
+
+Route::get('/tour', function () {
+    return view('tour');
+});
+
+Route::get('/hotel-single', function () {
+    return view('hotel-single');
+});
+
+Route::get('/blog-single', function () {
+    return view('blog-single');
+});
