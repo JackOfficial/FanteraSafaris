de7ed021308524e02d5b41aeee2627b74a8ff966
JackOfficial <musengimanajacques@gmail.com>
2026-02-24 22:55:31 +0200

    Home Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
 
     <meta property="og:title" content="Explore the Pearl of Africa with Fantera Safaris">
     <meta property="og:image" content="{{ asset('front/images/FanteraSafaris.png') }}">
+    <link href="{{ asset('front/images/FanteraSafaris.png') }}" rel="icon">
     
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet"> @include('partials.styles') </head>
