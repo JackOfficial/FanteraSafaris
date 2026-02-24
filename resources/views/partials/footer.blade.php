b183b4509dc8cc4949c23b2272b547ddc8f38213
JackOfficial <musengimanajacques@gmail.com>
2026-02-24 18:06:59 +0200

    Frontend

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,68 @@
-<div class="ftco-footer-widget mb-4">
-  <h2 class="ftco-heading-2 text-uppercase">Fantera Safaris</h2>
-  <p>Based in the heart of Kampala, we are your gateway to the untamed beauty of East Africa. From the mist-covered peaks of Bwindi to the endless plains of the Serengeti, we curate bespoke journeys for the discerning traveler.</p>
-  
-  <div class="trust-badges mt-4">
-      <img src="{{ asset('front/images/FanteraSafaris.png') }}" alt="Member of Association of Uganda Tour Operators" width="60">
-      <img src="{{ asset('front/images/FanteraSafaris.png') }}" alt="Uganda Tourism Board Licensed" width="60" class="ml-2">
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+  <div class="container">
+    <div class="row mb-5">
+      <div class="col-md">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">
+            <img src="{{ asset('front/images/FanteraSafaris.png') }}" alt="Fantera Safaris" style="max-height: 40px; filter: brightness(0) invert(1);">
+          </h2>
+          <p>Expertly crafted journeys across the "Pearl of Africa" and beyond. From the primates of Uganda to the vast plains of the Serengeti, we turn your African dreams into reality.</p>
+          <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+            <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+            <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+            <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md">
+        <div class="ftco-footer-widget mb-4 ml-md-5">
+          <h2 class="ftco-heading-2">Explore East Africa</h2>
+          <ul class="list-unstyled">
+            <li><a href="/destinations/uganda" class="py-2 d-block">Uganda Safaris</a></li>
+            <li><a href="/destinations/kenya" class="py-2 d-block">Kenya Expeditions</a></li>
+            <li><a href="/destinations/tanzania" class="py-2 d-block">Tanzania Tours</a></li>
+            <li><a href="/destinations/rwanda" class="py-2 d-block">Rwanda Trekking</a></li>
+            <li><a href="/safari-styles" class="py-2 d-block">Luxury Fly-in Safaris</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md">
+         <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Plan Your Trip</h2>
+          <ul class="list-unstyled">
+            <li><a href="/faq" class="py-2 d-block">Travel FAQ</a></li>
+            <li><a href="/booking-terms" class="py-2 d-block">Booking Conditions</a></li>
+            <li><a href="/packing-list" class="py-2 d-block">What to Pack</a></li>
+            <li><a href="/visa-info" class="py-2 d-block">Visa Information</a></li>
+            <li><a href="/contact" class="py-2 d-block">Custom Itineraries</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Have a Question?</h2>
+          <div class="block-23 mb-3">
+            <ul>
+              <li><span class="icon icon-map-marker"></span><span class="text">Plot XX, Kampala Road, Kampala, Uganda</span></li>
+              <li><a href="tel:+256XXXXXXXXX"><span class="icon icon-phone"></span><span class="text">+256 7xx xxxxxx</span></a></li>
+              <li><a href="mailto:info@fanterasafaris.com"><span class="icon icon-envelope"></span><span class="text">reservations@fanterasafaris.com</span></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <p class="mb-0">
+          Copyright &copy; {{ date('Y') }} All rights reserved | Fantera Safaris Ltd. 
+          <br>
+          <small class="text-muted">Licensed by Uganda Tourism Board & Member of AUTO</small>
+        </p>
+      </div>
+    </div>
   </div>
-</div>
+</footer>
