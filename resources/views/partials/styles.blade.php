0af2f850e0d0dd02ee99d5563c3e8da737de4721
JackOfficial <musengimanajacques@gmail.com>
2026-02-24 18:03:07 +0200

    Frontend

---
+++ resources/views/partials/styles.blade.php
@@ -0,0 +1,17 @@
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Alex+Brush" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet"> <link rel="stylesheet" href="{{ asset('front/css/open-iconic-bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('front/css/animate.css') }}">
+
+<link rel="stylesheet" href="{{ asset('front/css/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ asset('front/css/owl.theme.default.min.css') }}">
+<link rel="stylesheet" href="{{ asset('front/css/magnific-popup.css') }}">
+<link rel="stylesheet" href="{{ asset('front/css/aos.css') }}">
+<link rel="stylesheet" href="{{ asset('front/css/ionicons.min.css') }}">
+
+<link rel="stylesheet" href="{{ asset('front/css/bootstrap-datepicker.css') }}">
+<link rel="stylesheet" href="{{ asset('front/css/jquery.timepicker.css') }}">
+<link rel="stylesheet" href="{{ asset('front/css/flaticon.css') }}">
+<link rel="stylesheet" href="{{ asset('front/css/icomoon.css') }}">
+
+<link rel="stylesheet" href="{{ asset('front/css/style.css') }}">
