f4c9e60c568372adbc3bcfb9e409f8be762bbffe
JackOfficial <musengimanajacques@gmail.com>
2026-02-24 17:53:45 +0200

    Payment Orders

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="ftco-footer-widget mb-4">
+  <h2 class="ftco-heading-2 text-uppercase">Fantera Safaris</h2>
+  <p>Based in the heart of Kampala, we are your gateway to the untamed beauty of East Africa. From the mist-covered peaks of Bwindi to the endless plains of the Serengeti, we curate bespoke journeys for the discerning traveler.</p>
+  
+  <div class="trust-badges mt-4">
+      <img src="{{ asset('front/images/auto-logo.png') }}" alt="Member of Association of Uganda Tour Operators" width="60">
+      <img src="{{ asset('front/images/utb-logo.png') }}" alt="Uganda Tourism Board Licensed" width="60" class="ml-2">
+  </div>
+</div>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+  <div class="container">
+    <a class="navbar-brand font-weight-bold d-flex align-items-center" href="/">
+    <img src="{{ asset('front/images/FanteraSafaris.png') }}" 
+         alt="Fantera Safaris - Luxury East Africa Tours and Uganda Safaris" 
+         class="img-fluid safari-logo" 
+         style="max-height: 50px; width: auto;">
+</a>
+    
+    <div class="collapse navbar-collapse" id="ftco-nav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item {{ request()->is('/') ? 'active' : '' }}"><a href="/" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="/destinations" class="nav-link">Destinations</a></li>
+        <li class="nav-item"><a href="/safari-packages" class="nav-link">Safari Packages</a></li>
+        <li class="nav-item"><a href="/about-us" class="nav-link">Our Story</a></li>
+        <li class="nav-item"><a href="/contact" class="nav-link">Contact</a></li>
+        
+        <li class="nav-item cta ml-md-2">
+            <a href="https://wa.me/256xxxxxxxxx" class="nav-link bg-success border-0 text-white">
+                <span class="icon-whatsapp mr-1"></span> Chat with an Expert
+            </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
