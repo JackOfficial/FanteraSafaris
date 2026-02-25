8860ac2b1127a11d308220bf7407c1ead6167322
JackOfficial <musengimanajacques@gmail.com>
2026-02-25 17:50:27 +0200

    Fortify

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8">
-        <title>AutoSpareLink</title>
+        <title>FanteraSafasis</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="" name="keywords">
         <meta content="" name="description">
@@ -16,17 +16,10 @@
         <!-- Icon Font Stylesheet -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+        <link href="{{ asset('front/images/FanteraSafaris_logo.png') }}" rel="icon">
+         @include('partials.styles') 
 
-       <!-- Libraries Stylesheet -->
-        <link href="{{ asset('frontend/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('frontend/lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
-
-
-        <!-- Customized Bootstrap Stylesheet -->
-        <link href="{{ asset('frontend/css/bootstrap.min.css') }}" rel="stylesheet">
-
-        <!-- Template Stylesheet -->
-        <link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet">
+      
 
         <!-- Favicon -->
         <link href="{{ asset('frontend/img/logo.png') }}" rel="icon">
@@ -75,17 +68,7 @@
 
         
         <!-- JavaScript Libraries -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="{{ asset('frontend/lib/easing/easing.min.js') }}"></script>
-        <script src="{{ asset('frontend/lib/waypoints/waypoints.min.js') }}"></script>
-        <script src="{{ asset('frontend/lib/counterup/counterup.min.js') }}"></script>
-        <script src="{{ asset('frontend/lib/owlcarousel/owl.carousel.min.js') }}"></script>
-        <script src="{{ asset('frontend/lib/lightbox/js/lightbox.min.js') }}"></script>
-        
-
-        <!-- Template Javascript -->
-        <script src="{{ asset('frontend/js/main.js') }}"></script>
+         @include('partials.scripts')
         @livewireScripts
     </body>
 
