041f1e7d0b6974b7bb561de337fd53c30f066d44
JackOfficial <musengimanajacques@gmail.com>
2026-02-25 17:58:15 +0200

    Fortify

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -40,7 +40,7 @@
             <div class="container">
                 <div class="row g-4 align-items-center">
                     <div class="col-md-4 text-center text-md-start mb-md-0">
-                        <span class="text-body"><a href="#"><i class="fas fa-copyright text-light me-2"></i>AutoSpareLink</a>, All right reserved.</span>
+                        <span class="text-body"><a href="#"><i class="fas fa-copyright text-light me-2"></i>FanteraSafaris</a>, All right reserved.</span>
                     </div>
                     <div class="col-md-4 text-center">
                         <div class="d-flex align-items-center justify-content-center">
