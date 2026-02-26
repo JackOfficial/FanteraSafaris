0f8b4f80ba9bf799ab8deec14b64a88c582d501f
JackOfficial <musengimanajacques@gmail.com>
2026-02-26 06:00:29 +0200

    Fortify

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,68 +1,94 @@
-<footer class="ftco-footer ftco-bg-dark ftco-section">
+<footer class="ftco-footer ftco-section bg-dark text-white-50" style="padding-top: 5rem; padding-bottom: 3rem;">
   <div class="container">
-    <div class="row mb-5">
-      <div class="col-md">
+    <div class="row mb-5 justify-content-between">
+      
+      <div class="col-md-4">
         <div class="ftco-footer-widget mb-4">
-          <h2 class="ftco-heading-2">
-            <img src="{{ asset('front/images/FanteraSafaris.png') }}" alt="Fantera Safaris" style="max-height: 40px; filter: brightness(0) invert(1);">
+          <h2 class="ftco-heading-2 mb-4">
+            <img src="{{ asset('front/images/FanteraSafaris.png') }}" 
+                 alt="Fantera Safaris" 
+                 style="max-height: 45px; filter: brightness(0) invert(1);">
           </h2>
-          <p>Expertly crafted journeys across the "Pearl of Africa" and beyond. From the primates of Uganda to the vast plains of the Serengeti, we turn your African dreams into reality.</p>
-          <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-            <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-            <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-            <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+          <p class="text-white-50" style="line-height: 1.8;">
+            Expertly crafted journeys across the "Pearl of Africa" and beyond. From the primates of Uganda to the vast plains of the Serengeti, we turn your African dreams into reality.
+          </p>
+          <ul class="ftco-footer-social list-unstyled mt-4 d-flex">
+            <li class="mr-3"><a href="#" class="d-flex align-items-center justify-content-center bg-white-10 rounded-circle" style="width: 40px; height: 40px; background: rgba(255,255,255,0.05); color: #fff;"><i class="fab fa-facebook-f"></i></a></li>
+            <li class="mr-3"><a href="#" class="d-flex align-items-center justify-content-center bg-white-10 rounded-circle" style="width: 40px; height: 40px; background: rgba(255,255,255,0.05); color: #fff;"><i class="fab fa-instagram"></i></a></li>
+            <li class="mr-3"><a href="#" class="d-flex align-items-center justify-content-center bg-white-10 rounded-circle" style="width: 40px; height: 40px; background: rgba(255,255,255,0.05); color: #fff;"><i class="fab fa-whatsapp"></i></a></li>
           </ul>
         </div>
       </div>
 
-      <div class="col-md">
-        <div class="ftco-footer-widget mb-4 ml-md-5">
-          <h2 class="ftco-heading-2">Explore East Africa</h2>
-          <ul class="list-unstyled">
-            <li><a href="/destinations/uganda" class="py-2 d-block">Uganda Safaris</a></li>
-            <li><a href="/destinations/kenya" class="py-2 d-block">Kenya Expeditions</a></li>
-            <li><a href="/destinations/tanzania" class="py-2 d-block">Tanzania Tours</a></li>
-            <li><a href="/destinations/rwanda" class="py-2 d-block">Rwanda Trekking</a></li>
-            <li><a href="/safari-styles" class="py-2 d-block">Luxury Fly-in Safaris</a></li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="col-md">
-         <div class="ftco-footer-widget mb-4">
-          <h2 class="ftco-heading-2">Plan Your Trip</h2>
-          <ul class="list-unstyled">
-            <li><a href="/faq" class="py-2 d-block">Travel FAQ</a></li>
-            <li><a href="/booking-terms" class="py-2 d-block">Booking Conditions</a></li>
-            <li><a href="/packing-list" class="py-2 d-block">What to Pack</a></li>
-            <li><a href="/visa-info" class="py-2 d-block">Visa Information</a></li>
-            <li><a href="/contact" class="py-2 d-block">Custom Itineraries</a></li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="col-md">
+      <div class="col-md-2">
         <div class="ftco-footer-widget mb-4">
-          <h2 class="ftco-heading-2">Have a Question?</h2>
+          <h2 class="ftco-heading-2 text-white" style="font-size: 1.1rem; font-weight: 700;">Destinations</h2>
+          <ul class="list-unstyled">
+            <li><a href="/destinations/uganda" class="py-1 d-block text-white-50 hover-gold">Uganda</a></li>
+            <li><a href="/destinations/kenya" class="py-1 d-block text-white-50 hover-gold">Kenya</a></li>
+            <li><a href="/destinations/tanzania" class="py-1 d-block text-white-50 hover-gold">Tanzania</a></li>
+            <li><a href="/destinations/rwanda" class="py-1 d-block text-white-50 hover-gold">Rwanda</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-2">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2 text-white" style="font-size: 1.1rem; font-weight: 700;">Plan Your Trip</h2>
+          <ul class="list-unstyled">
+            <li><a href="/faq" class="py-1 d-block text-white-50 hover-gold">Travel FAQ</a></li>
+            <li><a href="/visa-info" class="py-1 d-block text-white-50 hover-gold">Visa Info</a></li>
+            <li><a href="/packing-list" class="py-1 d-block text-white-50 hover-gold">What to Pack</a></li>
+            <li><a href="/contact" class="py-1 d-block text-white-50 hover-gold">Custom Quotes</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2 text-white" style="font-size: 1.1rem; font-weight: 700;">Contact Us</h2>
           <div class="block-23 mb-3">
-            <ul>
-              <li><span class="icon icon-map-marker"></span><span class="text">Plot 47, Kampala Road, Kampala, Uganda</span></li>
-              <li><a href="tel:+256708239010"><span class="icon icon-phone"></span><span class="text">+256 708 239010</span></a></li>
-              <li><a href="mailto:info@fanterasafaris.com"><span class="icon icon-envelope"></span><span class="text">info@fanterasafaris.com</span></a></li>
+            <ul class="list-unstyled">
+              <li class="mb-2 d-flex">
+                <i class="fas fa-map-marker-alt mr-3 text-warning"></i>
+                <span class="text">Plot 47, Kampala Road, Uganda</span>
+              </li>
+              <li class="mb-2">
+                <a href="tel:+256708239010" class="text-white-50 d-flex align-items-center">
+                  <i class="fas fa-phone mr-3 text-warning"></i>
+                  <span>+256 708 239010</span>
+                </a>
+              </li>
+              <li class="mb-2">
+                <a href="mailto:info@fanterasafaris.com" class="text-white-50 d-flex align-items-center">
+                  <i class="fas fa-envelope mr-3 text-warning"></i>
+                  <span>info@fanterasafaris.com</span>
+                </a>
+              </li>
             </ul>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="row">
-      <div class="col-md-12 text-center">
-        <p class="mb-0">
-          Copyright &copy; {{ date('Y') }} All rights reserved | Fantera Safaris Ltd. 
-          <br>
-          <small class="text-muted">Licensed by Uganda Tourism Board & Member of AUTO</small>
+    <hr style="border-color: rgba(255,255,255,0.05);">
+
+    <div class="row align-items-center">
+      <div class="col-md-6 text-center text-md-left mb-3 mb-md-0">
+        <p class="mb-0 small">
+          &copy; {{ date('Y') }} <strong>Fantera Safaris Ltd</strong>. All rights reserved.
         </p>
+      </div>
+      <div class="col-md-6 text-center text-md-right">
+        <div class="trust-icons">
+          <span class="mr-3 small text-white-50">Licensed by UTB & AUTO Member</span>
+          </div>
       </div>
     </div>
   </div>
+
+  <style>
+    .hover-gold:hover { color: #ffc107 !important; transition: 0.3s; text-decoration: none; }
+    .bg-white-10:hover { background: #ffc107 !important; transition: 0.3s; }
+  </style>
 </footer>
