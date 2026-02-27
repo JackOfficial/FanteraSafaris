74065782c67c50b2880c5341b62f6e7cb1fb7098
JackOfficial <musengimanajacques@gmail.com>
2026-02-27 19:18:37 +0200

    Spatie

--- resources/views/admin/safari_categories/edit.blade.php
+++ resources/views/admin/safari_categories/edit.blade.php
@@ -3,22 +3,6 @@
 @section('title', 'Edit Category: ' . $safariCategory->name)
 
 @section('content')
-<section class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="font-weight-bold">Edit Safari Category</h1>
-            </div>
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
-                    <li class="breadcrumb-item"><a href="{{ route('admin.safari-categories.index') }}">Categories</a></li>
-                    <li class="breadcrumb-item active">Edit</li>
-                </ol>
-            </div>
-        </div>
-    </div>
-</section>
 
 <section class="content">
     <div class="container-fluid">
