08afa58e96e5332263e0f0067b358f14bb7d74b4
JackOfficial <musengimanajacques@gmail.com>
2026-02-27 18:47:48 +0200

    Spatie

--- resources/views/admin/packages/edit.blade.php
+++ resources/views/admin/packages/edit.blade.php
@@ -14,6 +14,7 @@
 @section('content')
 <section class="content-header">
     <div class="container-fluid">
+
         <div class="row mb-2">
             <div class="col-sm-6">
                 <h1 class="font-weight-bold">Edit Safari Package</h1>
@@ -32,6 +33,17 @@
 
 <section class="content">
     <div class="container-fluid">
+
+        @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul class="mb-0">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
         <form action="{{ route('admin.packages.update', $package) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
