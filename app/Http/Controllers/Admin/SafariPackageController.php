e794011e45b7836951c4b5c5dd8814c249d96f4e
JackOfficial <musengimanajacques@gmail.com>
2026-03-01 16:33:21 +0200

    Spatie

--- app/Http/Controllers/Admin/SafariPackageController.php
+++ app/Http/Controllers/Admin/SafariPackageController.php
@@ -95,7 +95,7 @@ class SafariPackageController extends Controller
     public function edit(SafariPackage $package)
     {
         // Load relationships: itineraries (sorted) and all polymorphic photos
-        $package->load(['itineraries' => fn($q) => $q->orderBy('day_number'), 'photos', 'category']);
+        $package->load(['itineraries' => fn($q) => $q->orderBy('day_number'), 'photos', 'categories']);
         $categories = SafariCategory::orderBy('name')->get();
         
         return view('admin.packages.edit', compact('package', 'categories'));
