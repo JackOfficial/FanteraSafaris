a4637f2c5629c35369a0c31f2c4740a25843502f
JackOfficial <musengimanajacques@gmail.com>
2026-03-01 20:29:26 +0200

    Spatie

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/', function () { return view('index'); });
+Route::get('/', [HomeController::class, 'index']);
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
 
